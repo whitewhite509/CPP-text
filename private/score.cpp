e8dffc2659fa7c8e199b35727f41312bfdd0e61f
whitewhite509 <35161541+whitewhite509@users.noreply.github.com>
2018-03-18 01:46:42 +0800

    Update score.cpp

--- private/score.cpp
+++ private/score.cpp
@@ -10,7 +10,7 @@ Score::Score(string name,int math)
     this->name=name;
     this->math=math;
 }
-void Score::setALL(string name,int math)
+void Score::setALL(string name,int math)//把私人的參數回傳到 setall裡 但setall是不回傳的  
 {
     this->name=name;//舊換新
     this->math=math;
