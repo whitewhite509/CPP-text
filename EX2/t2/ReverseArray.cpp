d27f514c84ea4d0fbad4bf25947f9b945aaef64d
whitewhite509 <35161541+whitewhite509@users.noreply.github.com>
2018-05-14 13:09:15 +0800

    Update ReverseArray.cpp

--- EX2/t2/ReverseArray.cpp
+++ EX2/t2/ReverseArray.cpp
@@ -1,36 +1,53 @@
 #include<iostream>
 using namespace std;
+
 template<typename T>
-void reverse(T list[],T newlist[],int size)
+void reverse(const T list[], T newlist[], int size)
 {
-    for(int i=0, j=size-1;i<size;i++,j--)
-    {
-        newlist[j]==list[i];
-    }
+	for (int i = 0, j = size - 1; i<size; i++, j--)
+	{
+		newlist[j] = list[i];
+	}
 
 }
+template<typename T>
+void p(const T list[],int size)
+{
+    for (int i = 0; i<size; i++)
+	{
+		cout << list[i] << " ";
+	}
 
+}
 int main()
 {
-int size=6;
-double list[]={'a','b','c','d','e','f'};
-double newlist[size];
-reverse(list,newlist,size);
+	const int size=6;
+	
+	char list[] = {'a','b','c','d','e','f' };
+	char newlist[size];
+	reverse(list, newlist, size);
+    cout<<"The original array:";
+    p(list,size);
+    cout<<endl;
+    cout<<"The reversed array:";
+    p(newlist,size);
+    cout<<endl;
+	
+    
+	float list2[] = {1.1,2.2,3.3,4.4,5.5,6.6};
+	float newlist2[size];
+	reverse(list2, newlist2, size);
 
-for(int i=0;i<size;i++)
-{
-cout<<list[i]<<" ";
+	 cout<<"The original array:";
+    p(list2,size);
+    cout<<endl;
+    cout<<"The reversed array:";
+    p(newlist2,size);
+    cout<<endl;
+    
+	system("pause");
+	return 0;
 }
-
-
-
-float list2[]={1.1,2.2,3.3,4.4,5.5,6.6};
-float newlist2[size];
-reverse(list2,newlist2,size);
-
-    for(int i=0;i<size;i++)
-    {
-    cout<<list[i]<<" ";
-    }
-
-}
+  
+//請將課本範例7.7 ReverseArray.cpp改成template版本,請用T代表 
+//讓程式可以處理任何資料型態的反向輸出。
